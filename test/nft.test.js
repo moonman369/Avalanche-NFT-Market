5d48fb29b04db467b97fa09bbbcdd2ab835264c5
Ayan Maiti <ayan.m@w3dev.email>
2022-09-07 19:35:51 +0000

    Commit: Started writing test scripts

---
+++ test/nft.test.js
@@ -0,0 +1,7 @@
+const { ethers } = require("hardhat");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const { BigNumber } = require("ethers");
+
+const expect = chai.expect;
+chai.use(chaiAsPromised);
